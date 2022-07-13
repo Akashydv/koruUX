9aa26110c44c6994167c229d468ebf32d1fb48af
Akash Yadav <67957388+Akashydv@users.noreply.github.com>
2022-07-13 09:58:10 +0530

    Update filter.js

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -6,7 +6,7 @@ function filterTable() {
     table = document.getElementById("tableId");
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[1];
+      td = tr[i].getElementsByTagName("td")[2];
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
